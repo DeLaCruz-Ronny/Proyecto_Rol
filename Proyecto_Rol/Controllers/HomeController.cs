3acf5da62be9b9a4ca110f6c544214e3826f6169
Ronny <rmdpdelacruz@gmail.com>
2022-10-01 15:31:00 -0400

    agregandolaLista

---
+++ Proyecto_Rol/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto_Rol.Models;
+using System.Diagnostics;
+
+namespace Proyecto_Rol.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ventas()
+        {
+            return View();
+        }
+
+        public IActionResult Compras()
+        {
+            return View();
+        }
+
+        public IActionResult Clientes()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Proyecto_Rol/Data/Logica.cs
@@ -0,0 +1,17 @@
+﻿using Proyecto_Rol.Models;
+namespace Proyecto_Rol.Data
+{
+    public class Logica
+    {
+        public List<Usuario> ListaUsuarios()
+        {
+            return new List<Usuario>
+            {
+                new Usuario{Nombre = "Ronny",Correo = "adm@gmail.com",Clave = "123",Roles = new string[]{"Administrador"} },
+                new Usuario{Nombre = "Pedro",Correo = "sup@gmail.com",Clave = "123",Roles = new string[]{"Supervidor"} },
+                new Usuario{Nombre = "Juan",Correo = "Emp@gmail.com",Clave = "123",Roles = new string[]{"Empleado"} },
+                new Usuario{Nombre = "Naomi",Correo = "superEmp@gmail.com",Clave = "123",Roles = new string[]{"Supervidor","Empleado"} }
+            };
+        }
+    }
+}
